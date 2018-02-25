c01b19e563e5ebc2487948217baf5d5640c91921
Krzysztof Szczurowski <kszczurowski@absolute.com>
2018-02-24 16:23:36 -0800

    Initial commit to Week 7 Lab 4

---
+++ BCIT_COMP3618_Week7_Lab4/ConAppSingleInstance/MyApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppSingleInstance
+{
+    class MyApp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
